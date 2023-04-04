ce4ffa3e26d6e68d3e508fc844bb3a5071bf775b
Elias Dolinsek <elias.dolinsek@gmail.com>
2023-04-04 10:07:20 +0000

    implemented left sidebar

--- app.vue
+++ app.vue
@@ -1,9 +1,21 @@
 <template>
-  <div :class="{'dark': darkMode}">
-    <h1 class="text-red-500 dark:text-dim-600">Hello World</h1>
+  <div :class="{ 'dark': darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+          <div class="hidden md:block xs:col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+          <div class="bg-blue-500">Center</div>
+          <div class="bg-blue-500">Right</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-const darkMode = ref(true)
+const darkMode = ref(false)
 </script>
