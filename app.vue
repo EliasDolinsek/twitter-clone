10c66b8428d169aede04a21e85214f9da4a30eae
Elias Dolinsek <elias.dolinsek@gmail.com>
2023-04-04 09:19:57 +0000

    Tailwind setup

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div :class="{'dark': darkMode}">
+    <h1 class="text-red-500 dark:text-dim-600">Hello World</h1>
   </div>
 </template>
+
+<script setup>
+const darkMode = ref(true)
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-
-})
+export default {
+    css: ['~/assets/css/main.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+}
